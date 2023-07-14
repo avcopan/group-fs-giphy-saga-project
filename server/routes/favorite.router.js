b5bbd627b65ebaa94712e9978439b9cdedf2ef83
Andreas Copan <avcopan@gmail.com>
2023-07-14 15:28:03 -0400

    Creates GET/POST/DELETE routes for favorites

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,26 +1,56 @@
-const express = require('express');
+const express = require("express");
+const query = require("../queries/favorites.query");
 
 const router = express.Router();
 
-// return all favorite images
-router.get('/', (req, res) => {
-  res.sendStatus(200);
+/**
+ * @api {get} /favorite Get all GIFs in favorites
+ */
+router.get("/", async (_, res) => {
+  try {
+    const data = await query.getFavorites();
+    res.send(data);
+  } catch (error) {
+    handleError(error, res);
+  }
 });
 
-// add a new favorite
-router.post('/', (req, res) => {
-  res.sendStatus(200);
+/**
+ * @api {post} /favorite Add a GIF to favorites
+ * @apiBody {Object} An object with keys `id` (from GIPHY), `title`, and `url`
+ */
+router.post("/", async (req, res) => {
+  try {
+    await query.addFavorite(req.body);
+    res.sendStatus(201);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
+
+/**
+ * @api {delete} /favorite/:id Remove a GIF from favorites
+ * @apiParam {Number} id GIFs unique ID (from GIPHY)
+ */
+router.delete("/:id", async (req, res) => {
+  try {
+    await query.removeFavorite(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+    handleError(error, res);
+  }
 });
 
 // update given favorite with a category id
-router.put('/:favId', async (req, res) => {
+router.put("/:favId", async (req, res) => {
   // req.body should contain a category_id to add to this favorite image
   res.sendStatus(200);
 });
 
-// delete a favorite
-router.delete('/', (req, res) => {
-  res.sendStatus(200);
-});
+// helper functions
+const handleError = async (error, res) => {
+  console.error(error);
+  return res.sendStatus(500);
+};
 
 module.exports = router;
